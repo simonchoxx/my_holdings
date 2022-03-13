661a2c9a01fd1ad8cc703f098f49371374b406d8
Santiago Simoncelli <simoncellisantiago@gmail.com>
2022-03-13 12:51:27 -0300

    change image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
 import image from '../assets/image1.jpg';
 
 export const Header = () => {
 	return (
 		<div className="px-0">
-			<Image src={image} style={{ width: '100%', height: '50vh' }} />
+			<img
+				src={image}
+				style={{ width: '100%', height: '50vh' }}
+				alt="Imagen"
+			></img>
 		</div>
 	);
 };
